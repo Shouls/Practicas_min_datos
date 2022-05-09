86a985db72e1519a4c2c19cf3b540bd11d2c7ed3
Rafael Barboza Garza <rafabarbozsa@gmail.com>
2022-05-09 11:39:25 -0500

    Cambios_finales

--- P8_clasificación.py
+++ P8_clasificación.py
@@ -10,7 +10,7 @@ from scipy.stats import mode
 def get_cmap(n, name="hsv"):
     return plt.cm.get_cmap(name, n)
 
-def scatter_group_by(archivo: str, df: pd.DataFrame, x_column: str, y_column: str, label_column: str):
+def scatter_group_by(archivo: str, df: pd.DataFrame, x_column: str, y_column: str, label_column: str, ):
     fig, ax = plt.subplots(figsize=(15, 10))
     labels = pd.unique(df[label_column])
     cmap = get_cmap(len(labels) + 1)
@@ -18,8 +18,8 @@ def scatter_group_by(archivo: str, df: pd.DataFrame, x_column: str, y_column: st
         filter_df = df.query(f"{label_column} == '{label}'")
         ax.scatter(filter_df[x_column], filter_df[y_column], label=label, color=cmap(i), linewidths = 0.2)
     ax.legend()
-    plt.xlabel("Valor de o3")
-    plt.ylabel("Valor de no2")
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
     plt.savefig(archivo)
     plt.close()
 
@@ -46,20 +46,39 @@ df_limpio = pd.read_csv(".././PollutionSK/csv/south-korean-pollution-data_limpio
 df_limpio['date']= pd.to_datetime(df_limpio['date'], format ="%d-%m-%Y")
 df_limpio[' o3'] = pd.to_numeric(df_limpio[' o3'], errors='coerce')
 df_limpio[' no2'] = pd.to_numeric(df_limpio[' no2'], errors='coerce')
+df_limpio[' pm25'] = pd.to_numeric(df_limpio[' pm25'], errors='coerce')
+
+
 df_2 = df_limpio[['date', ' o3', ' no2']]
+df_3 = df_limpio[['date', ' pm25', ' o3']]
+
 df_2['label'] =  [str(fecha.month) for fecha in df_limpio['date']]
+df_3['label'] =  [str(fecha.month) for fecha in df_limpio['date']]
+
 df_2 = df_2.drop('date', axis=1)
+df_3 = df_3.drop('date', axis=1)
+df_2 = df_2.dropna()
+df_3 = df_3.dropna()
 
 
-list_t = [
+list_t2 = [
     (np.array(tuples[0:1]), tuples[2])
     for tuples in df_2.itertuples(index=False, name=None)
 ]
-puntos = [point for point, _ in list_t]
-labels = [label for _, label in list_t]
-
-k_vecinos_mc(puntos, labels, [np.array([15, 5]), np.array([3, 2]), np.array([1, 7]), np.array([2, 6])], 4)
-scatter_group_by(".././PollutionSK/imgs/clasifi_clust/clasification_por_mes.jpg", df_2, " o3", " no2", "label")
+puntos2 = [point for point, _ in list_t2]
+labels2 = [label for _, label in list_t2]
 
 
+list_t3 = [
+    (np.array(tuples[0:1]), tuples[2])
+    for tuples in df_3.itertuples(index=False, name=None)
+]
+puntos3 = [point for point, _ in list_t3]
+labels3 = [label for _, label in list_t3]
+
+
+k_vecinos_mc(puntos2, labels2, [np.array([15, 5]), np.array([3, 2]), np.array([1, 7]), np.array([2, 6])], 4)
+scatter_group_by(".././PollutionSK/imgs/clasifi_clust/clasification_por_mes_o3_no2.jpg", df_2, " o3", " no2", "label")
+k_vecinos_mc(puntos3, labels3, [np.array([10, 5]), np.array([5, 7]), np.array([4, 5]), np.array([1, 3])], 4)
+scatter_group_by(".././PollutionSK/imgs/clasifi_clust/clasification_por_mes_pm25_o3.jpg", df_3, " pm25", " o3", "label")
 
--- P9_clustering.py
+++ P9_clustering.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from typing import List
 import numpy as np
 
-def k_means(points: List[np.array], k: int):
+def k_means(points: List[np.array], k: int, xlab: str, ylab: str, titl:str, imgname: str):
     DIM = len(points[0])
     N = len(points)
     num_cluster = k
@@ -22,35 +22,48 @@ def k_means(points: List[np.array], k: int):
             y[i] = pred
             
     plt.figure(figsize = (15,10))
-    plt.title("K_means")
-    plt.xlabel("valor de o3")
-    plt.ylabel("valor de no2")
+    plt.title(titl)
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
     for kl in range(num_cluster):
         xp = x[y == kl, 0]
         yp = x[y == kl, 1]
         plt.scatter(xp, yp)
-    plt.savefig(".././PollutionSK/imgs/clasifi_clust/k_means.jpg")
+    plt.savefig(imgname)
     plt.close()
     return mean
-
-
 
 df_limpio = pd.read_csv(".././PollutionSK/csv/south-korean-pollution-data_limpio.csv", index_col= "Unnamed: 0")
 df_limpio['date']= pd.to_datetime(df_limpio['date'], format ="%d-%m-%Y")
 df_limpio[' o3'] = pd.to_numeric(df_limpio[' o3'], errors='coerce')
 df_limpio[' no2'] = pd.to_numeric(df_limpio[' no2'], errors='coerce')
-df_2 = df_limpio[['date', ' o3', ' no2']]
-df_2['label'] =  [str(fecha.month) for fecha in df_limpio['date']]
-df_2 = df_2.drop('date', axis=1)
-df_2 = df_2.dropna()
+df_limpio[' pm25'] = pd.to_numeric(df_limpio[' pm25'], errors='coerce')
 
-list_t = [
+
+df_2 = df_limpio[['date', ' o3', ' no2']]
+df_3 = df_limpio[['date', ' pm25', ' o3']]
+
+df_2['label'] =  [str(fecha.month) for fecha in df_limpio['date']]
+df_3['label'] =  [str(fecha.month) for fecha in df_limpio['date']]
+
+df_2 = df_2.drop('date', axis=1)
+df_3 = df_3.drop('date', axis=1)
+df_2 = df_2.dropna()
+df_3 = df_3.dropna()
+
+list_t2 = [
     (np.array(tuples[0:2]), tuples[2])
     for tuples in df_2.itertuples(index=False, name=None)
 ]
 
-points = [point for point, _ in list_t]
+list_t3 = [
+    (np.array(tuples[0:2]), tuples[2])
+    for tuples in df_3.itertuples(index=False, name=None)
+]
 
-k_m = k_means(points, 8)
+points2 = [point for point, _ in list_t2]
+points3 = [point for point, _ in list_t3]
 
+k_means(points2, 8, "o3", "no2", "K means o3 y no2", ".././PollutionSK/imgs/clasifi_clust/k_means_o3_no2.jpg")
+k_means(points3, 8, "pm25", "o3", "K means pm25 y o3", ".././PollutionSK/imgs/clasifi_clust/k_means_pm25_o3.jpg")
 
